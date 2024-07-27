59857b1ad2a9dfc240c7febb99101f897a62e732
Anant2929 <guptaanant53@gmail.com>
2024-07-27 13:03:40 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// require('dotenv').config({path:'./env'}) it breaks the consistency of code because at some places we are 
+//using import and here require thats why we will use import here in this case also
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB();
+
+
+
+/*
+import express from "express"
+
+const app = express();
+
+//Always remember Database is in another content opr at another place so it takes time in connecting with 
+//the database , therefore we use async await here
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error" , (error) =>{
+            console.log("error: " , error);
+            throw error
+        })
+
+        app.listen(process.env.PORT , () =>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+        
+    } catch (error) {
+        console.error("error: " , error)
+        throw error
+    }
+})()
+This was our first approach lets move on to othe better aproach in we make another file for this and 
+write this stuff there so that we can maintain cleanliness of the code
+*/
